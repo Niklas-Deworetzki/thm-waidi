39d4677a45a8030db25cd5c0986b17368368a300
Niklas Deworetzki <niklas.deworetzki@mni.thm.de>
2019-04-20 17:01:06 +0200

    Added comments.

--- plotting/plot.py
+++ plotting/plot.py
@@ -8,6 +8,7 @@ IDX_E = 2
 fibCache = {}
 
 def fib(n):
+	"""Calculates the n-th fibonacci number using a lookup table"""
 	if n <= 2:
 		return 1
 	if n in fibCache:
@@ -17,10 +18,47 @@ def fib(n):
 	return fibn
 
 def normalizeFibonacci(data):
+	"""Normalizes the input data using the fibonacci numbers.
+
+	The input data has to be the form of (x, y, error) where
+	y marks the value at a given position x and error is the amount of
+	error for this value:
+
+	f(x) = y ± error
+
+	Now y and error are divided by the x-th fibonacci number:
+
+	f(x) = (y / fib(x)) ± (error / fib(x))
+
+	The returned data has the same format as input.
+	"""
 	return [(x, y / fib(x), e / fib(x)) for (x, y, e) in data]
 
 
 def readFile(filename):
+	"""Reads a data file into memory.
+
+	A data file contains three rows worth of data 
+	seperated by a comma and any amount of space characters:
+	 - The first row represents the x data. This data is to be seen as 
+	   context, e.g. time or an input value
+	 - The second row represents the y data. This data is to be seen as
+	   the real value to the corresponding input
+	 - The third row represents the error in the measurement.
+
+	Together the three rows build a function value:
+	 f(x) = y ± error
+
+	Any blank lines will be ignored while reading the input.
+	Lines starting with two dashes (--) are counted as an comment
+	and will also be ignored.
+
+	Data, Comments and Blank Lines may contain any amount of 
+	whitespace characters, which are removed from the data while reading.
+
+	The returned data has the form of
+	 [(x, y, error)]
+	"""
 	with open(filename) as f:
 		return [tuple(line.split()) for line 
 				in f.readlines() 
@@ -28,6 +66,14 @@ def readFile(filename):
 					and not line.startswith('--')]
 
 def transformData(data):
+	"""Transforms input data switching the outer list and the inner tuples.
+
+	The transformation has the abstract form of
+	 [(x, y, error)] -> ([x], [y], [error])
+
+	During transformation x will be converted into an int whereas
+	y and error are converted into a floating point number.
+	"""
 	x_pos = np.array([int(x) for (x, _, _) in data])
 	y_val = np.array([float(y) for (_, y, _) in data])
 	error = np.array([float(e) for (_, _, e) in data])
@@ -49,11 +95,12 @@ dataPython  = transformData(readFile('python.result'))
 dataHaskell = transformData(readFile('haskell.result'))
 dataJava    = transformData(readFile('java.result'))
 
+# Divide every y value by the minimal y value.
 if(doNormalize):
 	minY = min([min(data[IDX_Y]) for data in [dataPython, dataHaskell, dataJava]])
-	dataPython  = (dataPython[IDX_X],  dataPython[IDX_Y]  / minY, dataPython[IDX_E])
-	dataHaskell = (dataHaskell[IDX_X], dataHaskell[IDX_Y] / minY, dataHaskell[IDX_E])
-	dataJava    = (dataJava[IDX_X],    dataJava[IDX_Y]    / minY, dataJava[IDX_E])
+	dataPython  = (dataPython[IDX_X],  dataPython[IDX_Y]  / minY, dataPython[IDX_E]  / minY)
+	dataHaskell = (dataHaskell[IDX_X], dataHaskell[IDX_Y] / minY, dataHaskell[IDX_E] / minY)
+	dataJava    = (dataJava[IDX_X],    dataJava[IDX_Y]    / minY, dataJava[IDX_E]    / minY)
 	
 
 drawRawPlot(ax, dataPython,  'yellow')
