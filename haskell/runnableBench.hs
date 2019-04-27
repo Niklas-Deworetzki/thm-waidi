a98b26bc5f149639e994bf1289897db250854100
Niklas Deworetzki <niklas.deworetzki@mni.thm.de>
2019-04-27 22:42:52 +0200

    Made haskells runnable strict.

--- haskell/runnableBench.hs
+++ haskell/runnableBench.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE BangPatterns #-}
+
 import System.Environment(getArgs)
 
 main = print . fib . read . head =<< getArgs
 
 fib :: Int -> Int
-fib n = if n <= 2 then 1 else fib (n-1) + fib (n-2)
+fib !n = if n <= 2 then 1 else fib (n-1) + fib (n-2)
+
